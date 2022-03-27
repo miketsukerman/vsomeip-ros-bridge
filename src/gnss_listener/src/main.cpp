b3ea2ac82fcd3ff43946909c37b3f77ae9ce20f1
Michael Tsukerman <miketsukerman@gmail.com>
2022-03-27 18:34:38 +0200

    sample ros node
    implement sample node to test GNSS topic

--- src/gnss_listener/src/main.cpp
+++ src/gnss_listener/src/main.cpp
@@ -1,26 +1,30 @@
 #include "rclcpp/rclcpp.hpp"
-#include "rclcpp/duration.hpp"
 #include "std_msgs/msg/string.hpp"
 
-#include <nlohmann/json.hpp>
+using std::placeholders::_1;
 
-using json = nlohmann::json;
-
-class GnssListener : public rclcpp::Node
+class GnssSubsriber : public rclcpp::Node
 {
 public:
-  
-  GnssListener() : Node("gnss_listener")
-  {
-  }
+    GnssSubsriber() : Node("GNSS_Subscriber")
+    {
+      subscription = this->create_subscription<std_msgs::msg::String>(
+      "GNSS", 10, std::bind(&GnssSubsriber::topic_callback, this, _1));
+    }
 
+private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "GNSS json: '%s'", msg->data.c_str());
+    }
 
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription;
 };
 
 auto main(int argc, char * argv[]) -> int
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<GnssListener>());
+  rclcpp::spin(std::make_shared<GnssSubsriber>());
   rclcpp::shutdown();
   return 0;
 }
