ae482f3e3cdf53a10cde5257e01684fff6e78035
Michael Tsukerman <miketsukerman@gmail.com>
2022-04-10 22:00:11 +0200

    init-client: start ros daemon
    start ros daemon in client docker

--- config/init-client.sh
+++ config/init-client.sh
@@ -5,6 +5,8 @@ routingmanagerd &
 
 source /src/install/setup.sh
 
-VSOMEIP_APPLICATION_NAME=gnss-client ros2 run gnss_bridge gnss-bridge & 
+ros2 daemon start 
+
+VSOMEIP_APPLICATION_NAME=gnss-client ros2 run gnss_bridge gnss-bridge &
 
 ros2 run gnss_listener gnss-listener
