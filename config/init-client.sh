7928cfb70add0c8b43e3950175eb1a81e6a08066
Michael Tsukerman <miketsukerman@gmail.com>
2022-04-10 13:32:37 +0200

    init-client: add listener
    Run GNSS topic listener in client container

--- config/init-client.sh
+++ config/init-client.sh
@@ -5,4 +5,6 @@ routingmanagerd &
 
 source /src/install/setup.sh
 
-VSOMEIP_APPLICATION_NAME=gnss-client ros2 run gnss_bridge gnss-client
+VSOMEIP_APPLICATION_NAME=gnss-client ros2 run gnss_bridge gnss-bridge & 
+
+ros2 run gnss_listener gnss-listener
